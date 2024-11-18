6125cbefbf88e652d9a2e3417e9c39c3b2374cd3
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-11-18 14:54:54 +0200

    ...

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,18 @@
-import { useParams, useNavigate } from 'react-router-dom';
+import {
+    useParams,
+    useNavigate,
+    useLocation,
+    useMatch,
+} from 'react-router-dom';
 function Movie() {
     const { id } = useParams();
     const navigate = useNavigate();
+
+    const v1 = useLocation();
+    console.log('useLocation', v1);
+    const v2 = useMatch('/home/');
+    console.log('useMatch', v2);
+
     return (
         <>
             <h1>this is a movie {id}</h1>
