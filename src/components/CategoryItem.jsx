74933285dba2b4c97d713f0aabb7b7b9eb7bc9cc
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-11-18 19:04:01 +0200

    s92

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+function CategoryItem(props) {
+    const {
+        idCategory,
+        strCategoryThumb,
+        strCategoryDescription,
+        strCategory,
+    } = props;
+    return (
+        <div className='card'>
+            <div className='card-image'>
+                <img
+                    src={strCategoryThumb}
+                    alt={strCategory}
+                />
+            </div>
+            <div className='card-content'>
+                <span className='card-title'>{strCategory}</span>
+                <p>{strCategoryDescription.slice(0, 60)}...</p>
+            </div>
+            <div class='card-action'>
+                <Link
+                    to={`/category/${idCategory}`}
+                    className='btn'
+                >
+                    Watch category
+                </Link>
+            </div>
+        </div>
+    );
+}
+export { CategoryItem };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from 'react';
+import { getAllCategories } from '../api';
+import { Preloader } from '../components/Preloader';
+import { CategoryList } from '../components/CategoryList';
+
 function Home() {
+    const [catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        getAllCategories().then((data) => {
+            console.log(data.categories);
+            setCatalog(data.categories);
+        });
+    }, []);
+
     return (
-        <div>
-            <h1>Hello from Home page</h1>
-        </div>
+        <>
+            {!catalog.length ? (
+                <Preloader />
+            ) : (
+                <CategoryList catalog={catalog} />
+            )}
+        </>
     );
 }
 
